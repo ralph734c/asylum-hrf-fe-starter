2ad6ef18e88984be754f9824dbe479175a83d7b7
Christopher Ralph <chris.734c@gmail.com>
2024-11-16 22:33:26 -0500

    👍 IMPROVE: Adjusted the scrollStep and scrollInterval values for smoother, slower, scrolling, added comments for clarity.

--- src/components/pages/Landing/index.jsx
+++ src/components/pages/Landing/index.jsx
@@ -6,38 +6,34 @@ import { useNavigate } from 'react-router-dom';
 import { useDownloadData } from '../../../hooks/useDownloadData.js';
 // import { decodeBase64 } from '../../../utils/decodeBase64.js'; - this doesn't seem needed, yet, but leaving in case.
 
-/**
- * TODO: Ticket 1:
- * Implement structure and styles of the Landing page using Tailwind
- * Implement any button functionality implied by the landing page screenshot example (tickets/examples)
- */
 export const LandingPage = () => {
   const navigate = useNavigate();
   const { downloadCSV } = useDownloadData();
 
   const scrollToTop = () => {
-    let scrollStep = -window.scrollY / 13; // Adjust the divisor for speed
+    let scrollStep = -window.scrollY / 25; // calculates the number of pixels to scroll during each interval, higher = slower.
     let scrollInterval = setInterval(() => {
       if (window.scrollY === 0) {
         clearInterval(scrollInterval);
       } else {
         window.scrollBy(0, scrollStep);
       }
-    }, 18); // Adjust the interval time for smoothness
+    }, 15); // how often the function runs in ms, higher runs less-frequently, lower is more smooth.
   };
 
   const handleReadMore = () => {
-    // TODO: navigate to the humanrightsfirst.org homepage
+    // navigate to the Human Rights First Organization's home page in a new tab.
     window.open('https://humanrightsfirst.org', '_blank');
   };
 
+  // a card component to create the images and titles with styling.
   const ImageCard = ({ src, alt, title, imgClassName }) => (
     <div className='flex-c gap-3'>
       <img src={src} alt={alt} className={imgClassName} />
       <h3>{title}</h3>
     </div>
   );
-
+  // An insight card component for the insight's section with styling.
   const InsightCard = ({ value, description }) => (
     <div className='flex-c-1 gap-12'>
       <div className='insights-details-header'>
@@ -48,7 +44,7 @@ export const LandingPage = () => {
       </div>
     </div>
   );
-
+  // Create buttons with a uniform look via this component.
   const Button = ({ onClick, children, className }) => (
     <button onClick={onClick} className={`bg-[#aaa] px-[10px] py-[5px] text-white text-md font-semibold ${className}`}>
       {children}
@@ -56,7 +52,9 @@ export const LandingPage = () => {
   );
 
   return (
-    <div className='flex-c w-[100vw] secondary-c'>
+    // the parent div
+    <div className='flex-c w-[100vw] secondary-c'> 
+      {/* the top section */}
       <section className='primary-c flex pt-4 pb-8'>
         <div className='flex-c mx-auto'>
           <h1 className='text-6xl mb-8 text-white'>Asylum Office Grant Rate Tracker</h1>
@@ -66,6 +64,7 @@ export const LandingPage = () => {
           </h3>
         </div>
       </section>
+      {/* the graph's section */}
       <section className='flex-c pt-10'>
         <div className='flex-c'>
           <div className='flex justify-center m-14 gap-20 text-2xl'>
@@ -79,6 +78,7 @@ export const LandingPage = () => {
           </div>
         </div>
       </section>
+      {/* the middle section */}
       <section className='flex'>
         <div className='flex-1 content-center p-20'>
           <img src={paperStack} alt='Human Rights First' className='rounded-2xl h-[70%] w-[100%]' />
@@ -91,6 +91,7 @@ export const LandingPage = () => {
           </p>
         </div>
       </section>
+      {/* The bottom, insights section. */}
       <section className='flex-c gap-16'>
         <div>
           <h3 className='text-5xl'>Systemic Disparity Insights</h3>
