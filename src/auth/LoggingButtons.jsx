d829ee0b7641ed321f9c6e52ef96045c9d8aeb55
Christopher Ralph <chris.734c@gmail.com>
2024-11-24 18:54:07 -0500

    📦 NEW: Completed Auth0 functionality with login/out, profile page protected, stying for profile page

--- src/auth/LoggingButtons.jsx
+++ src/auth/LoggingButtons.jsx
@@ -1,32 +1,29 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
-/**
- * TODO: Ticket 3:
- * Implement authentication and logging functionality using Auth0
- */
 export const LoggingButtons = () => {
-  // TODO: Replace these with Auth0 functionality
+  // Destructure auth methods and properties
   const {
     isAuthenticated,
     loginWithRedirect,
     logout,
   } = useAuth0();
 
+  // Show log out only if user is authenticated
   const buttonText = isAuthenticated ? 'Log Out' : 'Log In';
 
+  // Ensure proper functionality is followed if authenticated, or not.
   const handleLogging = () => {
     if (isAuthenticated) {
-      // TODO: Add Logout functionality here:
       logout({
         returnTo: window.location.origin
       })
     } else {
-      // TODO: Add Redirect functionality here:
       loginWithRedirect()
     }
   };
 
+  // a dynamic button with text and actions based on auth status.
   return (
     <button className='nav-btn  px-4 py-1' onClick={handleLogging}>
       {buttonText}
--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,8 @@ const AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN;
 const AUTH_CLIENT_ID = import.meta.env.VITE_AUTH_CLIENT_ID;
 
 createRoot(document.getElementById('root')).render(
+  // Providing the Auth0 authentication functionality to the entire app
+  // Auth0 credentials come from .env.
   <Auth0Provider
     domain={AUTH_DOMAIN}
     clientId={AUTH_CLIENT_ID}
