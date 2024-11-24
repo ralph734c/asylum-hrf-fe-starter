ff678dbfc4797a0e5dbbca6fbbd70e697ae7046d
Christopher Ralph <chris.734c@gmail.com>
2024-11-23 22:30:27 -0500

    📦 NEW: Finished basic, first round, implementation of Auth0 on the logging buttons and confirmed its working

--- src/main.jsx
+++ src/main.jsx
@@ -7,13 +7,6 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN;
 const AUTH_CLIENT_ID = import.meta.env.VITE_AUTH_CLIENT_ID;
 
-/**
- * TODO: Ticket 3:
- * Implement authentication using Auth0:
- * - Wrap ProvideAppContext with the Auth Provider from Auth0
- * - Add your credentials from Auth0 to a .env file (AUTH_DOMAIN, AUTH_CLIENT_ID)
- * - Set the domain, clientId, and authorizationParams
- */
 createRoot(document.getElementById('root')).render(
   <Auth0Provider
     domain={AUTH_DOMAIN}
